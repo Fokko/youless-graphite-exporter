cc2f0206cf2e2e53c0f37a9c3463d80b90bec7b8
Fokko Driesprong <fokko@apache.org>
2019-08-04 21:12:09 +0200

    Make it less verbose

--- src/main/scala/frl/driesprong/youless/GraphiteClient.scala
+++ src/main/scala/frl/driesprong/youless/GraphiteClient.scala
@@ -4,7 +4,7 @@ import java.io._
 
 import javax.net.SocketFactory
 
-class GraphiteClient(address: String = Config.statsd, port:Int = 2003) extends Closeable {
+class GraphiteClient(address: String = Config.statsd, port: Int = 2003) extends Closeable {
 
   private lazy val socket = {
     val s = SocketFactory.getDefault.createSocket(address, port)
@@ -14,11 +14,8 @@ class GraphiteClient(address: String = Config.statsd, port:Int = 2003) extends C
 
   private lazy val writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream))
 
-  def send(name: String, value: String, timestamp: Long): Unit = {
-    val line = name + ' ' + value + ' ' + timestamp.toString + '\n'
-    println(line)
-    writer.write(line)
-  }
+  def send(name: String, value: String, timestamp: Long): Unit =
+    writer.write(name + ' ' + value + ' ' + timestamp.toString + '\n')
 
   /** Closes underlying connection. */
   def close() {
